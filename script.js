13e8e2b7742adeb9ab46518a18cb937a6e0ee0c2
TheSillySmoker <thesillysmoker@gmail.com>
2020-11-16 15:39:38 +1100

    ;lkj

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ var x = 0;
 
     switch(x){
         case 0:
-            meme = 'patMeme.jpg';
+            meme = 'patMeme.jpg';   
+             document.getElementById('meme').src= meme.text;
             break;
         case 1:
             meme = 'spongeMeme.jpg';
+            document.getElementById('meme').src= meme.text;
             break;
         case 2:
              meme = 'squidMeme.png'
+             document.getElementById('meme').src= meme.text;
             break;
         case 3: 
         meme = 'crabsMeme.jpg'
+        document.getElementById('meme').src= meme.text;
         
     }
     if (x == 4){
         x = 0;
     }
-    document.getElementById('meme').src= meme.text;
+
 }
